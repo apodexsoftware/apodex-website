4ea4a2d4badc3ebd9cd1ea2be1d6a00cb5ebef51
juan.goez-at-622901394401 <juan.goez@coraltech.co>
2025-08-13 22:32:43 -0500

    Configuracion para imagenes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // Necesario para export estático
+  },
+};
 
 export default nextConfig;
